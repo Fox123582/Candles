1e1491df27262edb7d0fabcc301e7cce7efdfaa1
leyka <bi4kax@gmail.com>
2025-03-02 20:09:14 +0200

    Styling

--- src/components/mainPage/mainContent/candlesContainer/candlesContainer.jsx
+++ src/components/mainPage/mainContent/candlesContainer/candlesContainer.jsx
@@ -5,12 +5,12 @@ import styles from './candlesContainer.module.css'
 export default function CandlesContainer() {
 
     const images = [
-        { src: 'src/assets/candlesPreview/accessories_2.svg', category: 'Аксессуары' },
-        { src: 'src/assets/candlesPreview/desert_1.svg', category: 'Десертные' },
-        { src: 'src/assets/candlesPreview/gel_1.svg', category: 'Гелевые' },
-        { src: 'src/assets/candlesPreview/innactive_1.svg', category: 'Inactive' },
-        { src: 'src/assets/candlesPreview/smth_1.svg', category: 'Соевые' },
-        { src: 'src/assets/candlesPreview/soy_1.svg', category: 'Мэлты' }
+        { src: 'src/assets/candlesPreview/accessories.png', category: 'Аксессуары' },
+        { src: 'src/assets/candlesPreview/desert.png', category: 'Десертные' },
+        { src: 'src/assets/candlesPreview/gel.png', category: 'Гелевые' },
+        { src: 'src/assets/candlesPreview/smth.png', category: 'Соевые' },
+        { src: 'src/assets/candlesPreview/innactive.png', category: 'Inactive' },
+        { src: 'src/assets/candlesPreview/soy.png', category: 'Мэлты' }
     ];
 
   return (
